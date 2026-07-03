4ee36d3542dca6c10768ac608ce1ed04fc851f55
ANTONYRAJ-maker <yantonyantony@gmail.com>
2026-07-03 15:57:51 +0530

    Add files via upload

---
+++ stpwatch.js
@@ -0,0 +1,82 @@
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+let timer = null;
+
+const display = document.getElementById("display");
+const laps = document.getElementById("laps");
+
+function updateDisplay(){
+
+let h = String(hours).padStart(2,"0");
+let m = String(minutes).padStart(2,"0");
+let s = String(seconds).padStart(2,"0");
+
+display.innerHTML = `${h}:${m}:${s}`;
+
+}
+
+function runStopwatch(){
+
+seconds++;
+
+if(seconds==60){
+seconds=0;
+minutes++;
+}
+
+if(minutes==60){
+minutes=0;
+hours++;
+}
+
+updateDisplay();
+
+}
+
+document.getElementById("start").onclick=function(){
+
+if(timer!==null)return;
+
+timer=setInterval(runStopwatch,1000);
+
+};
+
+document.getElementById("pause").onclick=function(){
+
+clearInterval(timer);
+
+timer=null;
+
+};
+
+document.getElementById("reset").onclick=function(){
+
+clearInterval(timer);
+
+timer=null;
+
+seconds=0;
+minutes=0;
+hours=0;
+
+updateDisplay();
+
+laps.innerHTML="";
+
+};
+
+document.getElementById("lap").onclick=function(){
+
+if(timer==null)return;
+
+let li=document.createElement("li");
+
+li.innerHTML=display.innerHTML;
+
+laps.appendChild(li);
+
+};
+
+updateDisplay();
